f7d81e41071fd9f3c8983232e9f23ac89b6a7af3
Hirakjyoti Talukdar <u20cse1041@cit.ac.in>
2023-06-30 00:58:51 +0530

    changes

--- nexacrawling/pipelines.py
+++ nexacrawling/pipelines.py
@@ -3,7 +3,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-
+#this is the main pipeline
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
